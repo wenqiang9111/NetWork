0fd17c4609de548d8e6fee43e60ec4b663ef0a60
wenqiang9111 <550752883@qq.com>
2016-03-31 17:19:42 +0800

    无

---
+++ ClientPoint/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] RecvBuf = new byte[1024];
+            int RecvBytes = 0;
+            string recvmsg = null;
+            while (true)
+            {
+                IPEndPoint remodteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6666);
+                TcpClient remoteServer = new TcpClient();
+                remoteServer.Connect(remodteEP);
+                string sendStr = Console.ReadLine();
+                byte[] SendBuf = Encoding.UTF8.GetBytes(sendStr);
+                remoteServer.Client.Send(SendBuf);
+
+                if (sendStr.Contains("886") || sendStr.Contains("再见"))
+                {
+                    RecvBytes = remoteServer.Client.Receive(RecvBuf);
+                    recvmsg = Encoding.UTF8.GetString(RecvBuf, 0, RecvBytes);
+                    if (recvmsg == "Exit!")
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(recvmsg);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServerPoint/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerPoint
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte[] SendBuf = Encoding.UTF8.GetBytes("Exit!");
+            IPEndPoint LocalEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6666);
+            TcpListener listener = new TcpListener(LocalEP);
+            listener.Start(10);
+
+            Console.WriteLine("Server is listening...");
+            while (true)
+            {
+                TcpClient remoteClient = listener.AcceptTcpClient();
+                Thread th = new Thread(new ThreadStart(() =>
+                {
+                    Console.WriteLine("Client:{0} connected!", remoteClient.Client.RemoteEndPoint.ToString());
+                    while (true)
+                    {
+                        byte[] RecvBuf = new byte[1024];
+                        int RecvBytes = 0;
+                        string recvmsg = null;
+
+                        RecvBytes = remoteClient.Client.Receive(RecvBuf);
+                        recvmsg = Encoding.UTF8.GetString(RecvBuf, 0, RecvBytes);
+                        Console.WriteLine("Client {0} says:{1}", remoteClient.Client.RemoteEndPoint.ToString(), recvmsg);
+                        if (recvmsg.Contains("886") || recvmsg.Contains("再见"))
+                        {
+                            Console.WriteLine("Client {0} 断开连接！", remoteClient.Client.RemoteEndPoint.ToString());
+                            break;
+                        }
+                    }
+                    remoteClient.Client.Send(SendBuf);
+                    remoteClient.Close();
+                }));
+                th.Start();
+            }
+        }
+    }
+}
